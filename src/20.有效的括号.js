866bac01bbfd2ce85ed6d77e6f1736d9725a436c
chengweiliang <chengweiliang@xin.com>
2019-12-03 18:28:00 +0800

    cod

---
+++ src/20.有效的括号.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=javascript
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (40.02%)
+ * Likes:    1224
+ * Dislikes: 0
+ * Total Accepted:    162K
+ * Total Submissions: 403.6K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 
+ * 有效字符串需满足：
+ * 
+ * 
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 
+ * 
+ * 注意空字符串可被认为是有效字符串。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "()"
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: "()[]{}"
+ * 输出: true
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: "(]"
+ * 输出: false
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: "([)]"
+ * 输出: false
+ * 
+ * 
+ * 示例 5:
+ * 
+ * 输入: "{[]}"
+ * 输出: true
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let len = s.length;
+    if (len & 1 === 0) {
+        let l = (l + len - 1) >>> 1;
+        let r = (l + len) >>> 1;
+        
+    } else {
+        return false;
+    }
+};
+// @lc code=end
+
