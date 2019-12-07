90e8860f9a2116ff2238df142eca172cc70095ee
sven36 <sven36@163.com>
2019-12-07 23:21:05 +0800

    cod

--- src/46.全排列.js
+++ src/46.全排列.js
@@ -36,12 +36,39 @@
  * @return {number[][]}
  */
 var permute = function (nums) {
-    let res = [];
-    let len = nums.length;
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-
+    // let res = [];
+    // let len = nums.length;
+    // let dp = [];
+    // dp[0] = [[nums[0]]];
+    // for (let i = 1; i < len; i++) {
+    //     dp[i] = [];
+    //     for (let j = 0; j <= dp[i - 1].length; j++) {
+    //         if (j === dp[i - 1].length) {
+    //             let clone = [...dp[i - 1][j - 1]];
+    //             clone.push(nums[i]);
+    //             dp[i].push(clone);
+    //         } else {
+    //             let clone = [...dp[i - 1][j]];
+    //             clone.splice(j, 0, nums[i]);
+    //             dp[i].push(clone);
+    //         }
+    //     }
+    // }
+    // return dp[n - 1]
+    const result = [];    // 结果
+    const f = (arr, vs) => {
+        if (vs.length === arr.length) {   // 若中间数组vs的长度等于原始数组长度则说明组完了
+            result.push(vs);
+            return;
+        }
+        const need = arr.filter(v => !vs.includes(v));  // 筛选可以放到当前中间数组后面的元素
+        for (const n of need) {
+            f(arr, [...vs, n]); // 更新中间数组
+        }
     }
+    f(nums, []);
+    return result;
 };
+// permute([1, 2, 3]);
 // @lc code=end
 
